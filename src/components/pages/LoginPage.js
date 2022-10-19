f180e0196a1e219f49560eb2dca158eb5ce92e0e
unknown <hp4129889@gmail.com>
2022-10-19 14:05:38 +0800

    frist commit

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 import '../../App.css'
 
 export default function SignInPage() {
+    const [formData, setFormData] = useState();
+
+    const login = async e => {
+        e.preventDefault();
+        const url = 'http://localhost:3002/login';
+        const res = await axios({
+            method: 'POST',
+            url: url,
+            data: formData
+        });
+        console.log(res);
+    };
+
+    const handleChange = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    };
+
     return (
         <div className="text-center m-5-auto">
             <h2>Sign in to us</h2>
-            <form action="/home">
+            <form onSubmit={login}>
                 <p>
-                    <label>Username or email address</label><br/>
-                    <input type="text" name="first_name" required />
+                    <label>Username or email address</label><br />
+                    <input type="text" name="username" onChange={handleChange} />
                 </p>
                 <p>
                     <label>Password</label>
                     <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
-                    <br/>
-                    <input type="password" name="password" required />
+                    <br />
+                    <input type="password" name="password" onChange={handleChange} />
                 </p>
                 <p>
                     <button id="sub_btn" type="submit">Login</button>
